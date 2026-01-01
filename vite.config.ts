e680dba7fab0a21eba0065b014f393b743432805
tiodio324 <nikita324smirnovvv@gmail.com>
2026-01-01 17:24:53 +0300

    Дипломка. Смирнов Н.А.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ClassSchedule/',
+  base: '/ClassSchedule-13/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
